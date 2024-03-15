84dadada6ed6b0cc7327e08e0a65d5896998e7e2
Terry Babyuk <tbabyuk@gmail.com>
2024-03-15 01:46:42 -0400

    payrablerow formatting improved

--- app/components/PayTableRow.jsx
+++ app/components/PayTableRow.jsx
@@ -23,9 +23,13 @@ export const PayTableRow = ({metaDoc, handleAttendanceModal, handleNotesModal}) 
   return (
     <tr className="even:bg-gray-300">
         <td className="py-[10px] px-3 sm:px-6">{teacher[0].toUpperCase() + teacher.slice("1")}</td>
+
         <td className="py-[10px] px-3 sm:px-6">{!week2Submitted ? <MdRadioButtonUnchecked size="1.2rem" className="mx-auto" /> : <MdRadioButtonChecked size="1.2rem" color="green" className="mx-auto" />}</td>
+
         <td className="py-[10px] px-3 sm:px-6 text-nowrap">{formatDate()}</td>
+        
         {authenticatedUser?.displayName !== "Heather" && <td className="py-[10px] px-3 sm:px-6">{!week2Submitted ? "n/a" : totalPay && "$" + totalPay.toFixed(2)}</td>}
+
         {authenticatedUser?.displayName !== "Heather" && <td className="px-3 sm:px-6 text-nowrap text-center">
             <button className="table-btn" disabled={!week2Submitted} onClick={() => handleAttendanceModal(teacher)}>view</button>
         </td>}
